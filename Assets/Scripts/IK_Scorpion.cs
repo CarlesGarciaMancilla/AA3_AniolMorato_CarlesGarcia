4dc0dae26bce35988d59a692a6e0d4e36af892ae
Aniol Morató <aniol.morato@enti.cat>
2024-01-14 12:28:20 +0100

    Actualitza posició cos
    Diria que esta be, pero costa una mica de veure

--- Assets/Scripts/IK_Scorpion.cs
+++ Assets/Scripts/IK_Scorpion.cs
@@ -26,12 +26,20 @@ public class IK_Scorpion : MonoBehaviour
     public Transform[] legTargets;
     public Transform[] futureLegBases;
 
+    [Header("Update Body Position")]
+    public Transform[] joints;
+    public Vector3 startingPosition;
+    public float startingYPosition;
+    public Vector3 temporaryPositions;
+    public Vector3 updatedPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         _myController.InitLegs(legs,futureLegBases,legTargets);
         _myController.InitTail(tail);
 
+        startingYPosition = joints[0].transform.position.y;
     }
 
     // Update is called once per frame
@@ -52,6 +60,7 @@ public class IK_Scorpion : MonoBehaviour
         if (animTime < animDuration)
         {
             Body.position = Vector3.Lerp(StartPos.position, EndPos.position, animTime / animDuration);
+            updateBodyPosition();
         }
         else if (animTime >= animDuration && animPlaying)
         {
@@ -73,5 +82,17 @@ public class IK_Scorpion : MonoBehaviour
     {
 
         _myController.NotifyStartWalk();
+    }
+
+    private void updateBodyPosition()
+    {
+        if (startingPosition.y != joints[0].transform.localPosition.y)
+        {
+            temporaryPositions.y = joints[0].transform.position.y - startingYPosition;
+            startingPosition = joints[0].transform.localPosition;
+        }
+        updatedPosition = Body.localPosition;
+        updatedPosition.y = temporaryPositions.y;
+        Body.localPosition = updatedPosition;
     }
 }
