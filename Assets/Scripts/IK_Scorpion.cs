c0a7545fd104d147a5a0839dee12ff64fde0dc08
Aniol Morató <aniol.morato@enti.cat>
2024-01-14 16:13:05 +0100

    Actualització rotació a mitges
    DIria que el codi esta be, pero no aconsegueixo que s'executi sense liar les potes per alguna raó

--- Assets/Scripts/IK_Scorpion.cs
+++ Assets/Scripts/IK_Scorpion.cs
@@ -33,6 +33,9 @@ public class IK_Scorpion : MonoBehaviour
     public Vector3 temporaryPositions;
     public Vector3 updatedPosition;
 
+    [Header("Update Body Rotation")]
+    public int a;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -60,7 +63,8 @@ public class IK_Scorpion : MonoBehaviour
         if (animTime < animDuration)
         {
             Body.position = Vector3.Lerp(StartPos.position, EndPos.position, animTime / animDuration);
-            updateBodyPosition();
+            UpdateBodyPosition();
+            //UpdateBodyRotation();
         }
         else if (animTime >= animDuration && animPlaying)
         {
@@ -84,7 +88,7 @@ public class IK_Scorpion : MonoBehaviour
         _myController.NotifyStartWalk();
     }
 
-    private void updateBodyPosition()
+    private void UpdateBodyPosition()
     {
         if (startingPosition.y != joints[0].transform.localPosition.y)
         {
@@ -94,5 +98,44 @@ public class IK_Scorpion : MonoBehaviour
         updatedPosition = Body.localPosition;
         updatedPosition.y = temporaryPositions.y;
         Body.localPosition = updatedPosition;
+    }
+
+    private void UpdateBodyRotation()
+    {
+
+        //Initialize vectors for leg pos
+        Vector3 rightLegs = new Vector3(0, 0, 0);
+        Vector3 leftLegs = new Vector3(0, 0, 0);
+
+
+        //Add legs to joints
+        for (int i = 0; i < 6; i++)
+        {
+            //Right legs only
+            if (i == 0 || i == 2 || i == 4)
+            {
+                rightLegs += joints[i].transform.position;
+            }
+            //Left legs only
+            else if (i == 1 || i == 3 || i == 5)
+            {
+                leftLegs += joints[i].transform.position;
+            }
+        }
+
+        if (rightLegs.y == leftLegs.y)
+        {
+            Body.transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, 0);
+        }
+        else if (leftLegs.y > rightLegs.y)
+        {
+            float legHeight = leftLegs.y - rightLegs.y;
+            Body.transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z + legHeight * 5);
+        }
+        else if (rightLegs.y > leftLegs.y)
+        {
+            float legHeight = rightLegs.y - leftLegs.y;
+            Body.transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z - legHeight * 5);
+        }
     }
 }
